a8ae4eae2c7c93a9de2f353e64dc6fda3bc3c50b
Masato Ikeda <masato.ikeda@gmail.com>
2019-11-21 01:34:13 +0900

    Run migrate

--- kauth/db/schema.rb
+++ kauth/db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_20_162757) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
